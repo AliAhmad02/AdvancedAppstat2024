ef5405edd4a5f4f9524de57e9c894a493f11d6d2
Ali Ahmad <aliahmaddk@gmail.com>
2024-02-26 02:03:44 +0100

    Add solution to exercise 2 in problem set 2.

---
+++ Problem sets/Problem set 2/ex2.py
@@ -0,0 +1,54 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import gamma
+
+
+# Requires scienceplots style installation
+import scienceplots
+
+plt.style.use(["science"])
+
+
+x_min = 0
+x_mid = 1.6
+x_max = 2.1
+N_lin_prior = 1000
+
+lower = 0
+upper = 4
+x = np.linspace(lower, upper, N_lin_prior)
+norm_prior = 2.93
+prior = np.piecewise(
+    x,
+    [
+        (x > x_min) & (x <= x_mid),
+        (x > x_mid) & (x <= x_max),
+        (x <= x_min) | (x > x_max),
+    ],
+    [1 / norm_prior, 2.66 / norm_prior, 0],
+)
+
+alpha = 2
+beta = 2
+likelihood = gamma.pdf(x, a=alpha, scale=1 / beta)
+posterior = prior * likelihood
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(x, prior, label=f"Prior", color="aqua")
+ax.plot(x, likelihood, label=f"Likelihood", color="dodgerblue")
+ax.plot(
+    x,
+    posterior,
+    label=f"Posterior",
+    color="midnightblue",
+)
+ax.set_xlabel("x", fontsize=15)
+ax.set_ylabel("Probability", fontsize=15)
+plt.legend(frameon=False, fontsize=13)
+plt.show()
+
+max_posterior_idx = np.argmax(posterior)
+max_posterior_x = x[max_posterior_idx]
+max_posterior_val = posterior[max_posterior_idx]
+
+print(f"\nThe most like estimate is P(x={max_posterior_x:.2f})={max_posterior_val:.2f}")
